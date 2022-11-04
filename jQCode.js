0423cf773e6b59de67ae67aa0a0495e98eac2b86
GHTeodor <Theodor.Lviv@gmail.com>
2022-11-05 00:54:24 +0200

    jQuery

---
+++ jQCode.js
@@ -0,0 +1,50 @@
+jQuery(document).ready(function () {
+    // $('h1.mainText').text("JQuery").hide(1500).show(1500).css({
+    //     'border': 'double cornflowerblue thick'
+    // }).animate({
+    //     'padding-left': '20px'
+    // }, 1000);
+    //
+    // //
+    //
+    // function widthAndHeight(element) {
+    //     const className = `.${element}`;
+    //     const [w, h] = [$(className).width(), $(className).height()];
+    //     const pText = $('p.pText');
+    //     pText.css('color', 'pink').text('Width: ' + w + '; Height: ' + h + '; ' + pText.css('color')).fadeOut(3000).fadeIn(1000);
+    // }
+    // //
+    // function elementOut(element, time) {
+    //     if (!(time > 5000 || time < 1000 || isNaN(time))) {
+    //         $(`.${element}`).fadeOut(time);
+    //     }
+    // }
+    // //
+    // $('p.pText2').fadeTo(2000, .5, elementOut('pText1', 2000)).fadeTo(2000, 1, widthAndHeight('mainText'));
+    // //
+    // $('p.pText3').slideUp(1000).slideDown(1000);
+    // //
+
+    // $('div.small a').click(function (e) {
+    //     if ($('div.big img').attr('src') !== $(this).attr('href')) {
+    //         $('div.big img').hide().attr('src', $(this).attr('href')).fadeIn('1s');
+    //     }
+    //     e.preventDefault();
+    // });
+    //
+    // $('button').click(function () {
+    //     $('div.gallery').slideToggle(500);
+    //     $('button').text() === '↥'
+    //         ? $('button').text('↧')
+    //         : $('button').text('↥');
+    // });
+    //
+    // $('div.small a img').click(function () {
+    //     $('div.small a img').fadeTo(500, 1).css({
+    //         'border': 'none'
+    //     });
+    //     $(this).fadeTo(500, .6).css({
+    //         'border': '1px dotted red'
+    //     });
+    // });
+});
